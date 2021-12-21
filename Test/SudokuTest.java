008a7aa6d7518c202a2aef3032eca37a72b3d89b
jasonli8408 <jasonli8408@gmail.com>
2021-12-20 19:27:40 -0500

    test if needed

---
+++ Test/SudokuTest.java
@@ -0,0 +1,19 @@
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SudokuTest{
+    public char[][] board = new char[9][9];
+
+
+    @Test
+    public void testSudoku(){
+        char[][] actual = new char[9][9];
+        char[][] expected = new char[9][9];
+
+    }
+}
