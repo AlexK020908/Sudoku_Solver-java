6999d64714811f9451472d6137af58085edb7834
jasonli8408 <jasonli8408@gmail.com>
2021-12-20 22:02:03 -0500

    updates boi

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,23 +1,14 @@
+import java.util.ArrayList;
+
 public class Sudoku {
 
     public static final int n = 3;
     public static final int N = n * n;
 
-
-    //we have a total of 9 boxes
-    //each box has 9 components
-    private int[] box;
-    //9 rows with each row has 9 indexes
-    private int[] rows;
-    //9 columns, each column with 9 indexes
-    private int[] columns;
     //represents a 9 by 9 board. first bracket is column, second is row
     private char[][] board;
 
     public Sudoku(char[][] board) {
-        box = new int[N];
-        rows = new int[N];
-        columns = new int[N];
         this.board = board;
     }
 
@@ -33,40 +24,39 @@ public class Sudoku {
         for (int i = 0 ; i < N ; i++) {
             if (board[row][i] == num) return false;
             if (board[i][column] == num) return false;
-            if (containedInBox(board, startRow, startColumn,num)) return false;
+            if (itemsContainedInBox(board, startRow, startColumn).contains((char) num)) return false;
         }
 
         return true;
     }
 
     //EFFECT: returns true if the number is contained in the box
-    private boolean containedInBox(char[][] board, int startRow, int startColumn, int num) {
+    private ArrayList<Character> itemsContainedInBox(char[][] board, int startRow, int startColumn) {
         int endRow = startRow + 2;
         int endColumn = startColumn + 2;
+        ArrayList<Character> items = new ArrayList<>();
 
         for (int i = startRow ; i <= endRow ; i++) {
             for (int j = startColumn ; j <= endColumn ; j++) {
-                if (board[i][j] == num) {
-                    return true;
+                if (board[i][j] != '.') {
+                    items.add(board[i][j]);
                 }
             }
         }
 
-        return false;
+        return items;
     }
 
 
     //effect: get the start column
     private int getStartColumn(int boxIndex) {
-        int start= 0;
         if (boxIndex % 3 == 0) {
-            start = 0;
-        } else if (boxIndex % 3 == 1 || boxIndex == 1) {
-            start = 3;
-        } else if (boxIndex % 3 == 2) {
-            start = 6;
+            return 0;
+        } else if (boxIndex % 3 == 1) {
+            return 3;
+        } else { // (boxIndex % 3 == 2)
+            return 6;
         }
-        return start;
     }
 
 
@@ -77,15 +67,14 @@ public class Sudoku {
 
     //effect: get the start row
     private int getStartRow(int boxIndex) {
-        int startRow = 0;
+
         if (boxIndex % 3 == 0) {
-            startRow = boxIndex;
-        } if (boxIndex % 3 == 1) {
-            startRow = boxIndex - 1;
-        } if (boxIndex % 3 == 2) {
-            startRow = boxIndex - 2;
+            return boxIndex;
+        } else if (boxIndex % 3 == 1) {
+            return boxIndex - 1;
+        } else { //(boxIndex % 3 == 2)
+            return boxIndex - 2;
         }
-        return startRow;
     }
 
     //EFFECT: place a valid number at the specified row and column only if the placement is allowed
@@ -97,7 +86,7 @@ public class Sudoku {
 
 
     //EFFECT: remove the number placed at specified row and column .
-    public void removePlacement(int row, int column, int num, char[][] board) {
+    public void removePlacement(int row, int column, char[][] board) {
         board[row][column] = '.';
     }
 
